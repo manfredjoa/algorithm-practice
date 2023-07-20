1cc9f62b2bdf489721b67cd61b40de651d494bfe
manfredjoa <joamanfred@gmail.com>
2023-07-19 22:55:57 -0400

    update array-last script.js

--- array-methods/array-last/script.js
+++ array-methods/array-last/script.js
@@ -1,5 +1,6 @@
 // Prompt
-// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element.
+// If there are no elements in the array, it should return -1.
 
 Array.prototype.last = function () {
   if (this.length === 0) {
