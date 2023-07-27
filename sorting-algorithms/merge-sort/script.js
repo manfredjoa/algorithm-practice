b27b26ba394913d35dd84eadf7bd7dc808cb6d72
manfredjoa <joamanfred@gmail.com>
2023-07-27 09:18:53 -0400

    add merge sort algorithm

---
+++ sorting-algorithms/merge-sort/script.js
@@ -0,0 +1,42 @@
+// Merges two sorted arrays
+function merge(arr1, arr2) {
+  const results = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  return results;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr; // base case
+
+  let mid = Math.floor(arr.length / 2);
+
+  let left = mergeSort(arr.slice(0, mid)); //recursive case
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+// console.log(mergeSort([14, 22, 78, 63, 74, 1, 10]));
