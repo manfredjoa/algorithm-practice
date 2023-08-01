f6de2a23ba2d73d1dcab7e21b7b861f8d11f653e
manfredjoa <joamanfred@gmail.com>
2023-08-01 09:41:02 -0400

    add prompt for palindromeNumber

--- LeetCode/palindromeNumber.js
+++ LeetCode/palindromeNumber.js
@@ -1,3 +1,6 @@
+// Prompt
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
 const isPalindrome = (x) => {
   if (x.toString().split("").reverse().join("") == x) {
     return true;
