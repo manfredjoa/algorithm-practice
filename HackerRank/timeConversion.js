de6baf87d32cd93ac6e7303679f9659e636ddb14
manfredjoa <joamanfred@gmail.com>
2023-09-08 10:31:27 -0400

    add problem

--- HackerRank/timeConversion.js
+++ HackerRank/timeConversion.js
@@ -1,3 +1,8 @@
+// Problem
+// Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
 function timeConversion(s) {
   if (s.at(-2) === "A") {
     if (s.slice(0, 2) === "12") return `00${s.slice(2, -2)}`;
