89202f581d8b8e614ab2863563660dba976606ad
manfredjoa <joamanfred@gmail.com>
2023-09-11 18:35:31 -0400

    add problem description, add more efficient solution

--- HackerRank/Week 2/salesByMatch.js
+++ HackerRank/Week 2/salesByMatch.js
@@ -1,4 +1,6 @@
 // Problem
+// There is a large pile of socks that must be paired by color.
+// Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 function sockMerchant(n, ar) {
   let pairs = 0;
@@ -12,6 +14,19 @@ function sockMerchant(n, ar) {
         i--;
         break;
       }
+    }
+  }
+  return pairs;
+}
+
+// More efficient solution
+function sockMerchant(n, ar) {
+  let sortedArray = ar.sort((a, b) => a - b);
+  let pairs = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (sortedArray[i] === sortedArray[i + 1]) {
+      pairs++, i++;
     }
   }
   return pairs;
