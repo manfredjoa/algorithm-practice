00a1bd4b5d12e51c209b581575a1487f96ce0d03
manfredjoa <joamanfred@gmail.com>
2023-09-20 16:02:08 -0400

    add plusOne.js

---
+++ LeetCode/Easy/plusOne.js
@@ -0,0 +1,11 @@
+// Prompt
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+// The digits are ordered from most significant to least significant in left-to-right order.
+// The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+const plusOne = (digits) => {
+  let num = BigInt(digits.join("")) + 1n;
+  return num.toString().split("");
+};
