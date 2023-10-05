9866c2d727027bc78de3b8b33d1764f0ff7b5ea6
manfredjoa <joamanfred@gmail.com>
2023-10-05 14:20:42 -0400

    add deepestLeavesSum.js

---
+++ LeetCode/Medium/deepestLeavesSum.js
@@ -0,0 +1,27 @@
+// Prompt
+// Given the root of a binary tree, return the sum of values of its deepest leaves.
+
+const deepestLeavesSum = (root) => {
+  let maxDepth = 0;
+  let sum = 0;
+
+  function helper(node, depth) {
+    if (!node) return;
+
+    if (depth > maxDepth) {
+      maxDepth = depth;
+      sum = 0;
+    }
+
+    if (depth === maxDepth) {
+      sum += node.val;
+    }
+
+    helper(node.left, depth + 1);
+    helper(node.right, depth + 1);
+  }
+
+  helper(root, 1);
+
+  return sum;
+};
