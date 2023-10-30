d2cdd5852b3cf171226942ddfbef2a203b1e7c2d
manfredjoa <joamanfred@gmail.com>
2023-10-30 14:33:00 -0400

    add topKFrequentElements.js

---
+++ LeetCode/Medium/topKFrequentElements.js
@@ -0,0 +1,19 @@
+// Prompt
+// Given an integer array nums and an integer k, return the k most frequent elements.
+// You may return the answer in any order.
+
+const topKFrequent = (nums, k) => {
+  let frequencies = {};
+  let result = [];
+
+  for (const i in nums) {
+    if (!frequencies[nums[i]]) frequencies[nums[i]] = 1;
+    else frequencies[nums[i]]++;
+  }
+
+  let frequenciesArr = Object.entries(frequencies).sort((a, b) => b[1] - a[1]);
+
+  for (let i = 0; i < k; i++) result.push(parseInt(frequenciesArr[i][0]));
+
+  return result;
+};
