2056e92db7b187e5624fa0105906587156db93b2
manfredjoa <joamanfred@gmail.com>
2023-11-21 15:12:13 -0500

    add iterative solution

--- LeetCode/Easy/symmetricTree.js
+++ LeetCode/Easy/symmetricTree.js
@@ -2,7 +2,7 @@
 // Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
 // Recursive Solution
-const isSymmetric = (root) => {
+const isSymmetricRecursive = (root) => {
   const helper = (left, right) => {
     if (!left && !right) return true;
     else if ((!left && right) || (left && !right) || left.val !== right.val)
@@ -15,4 +15,25 @@ const isSymmetric = (root) => {
   };
 
   return helper(root.left, root.right);
+};
+
+// Iterative Solution
+const isSymmetricIterative = (root) => {
+  if (!root) return true;
+
+  const queue = [root.left, root.right];
+
+  while (queue.length > 0) {
+    const left = queue.shift();
+    const right = queue.shift();
+
+    if (!left && !right) continue;
+    else if ((!left && right) || (left && !right) || left.val !== right.val)
+      return false;
+
+    queue.push(left.left, right.right);
+    queue.push(left.right, right.left);
+  }
+
+  return true;
 };
