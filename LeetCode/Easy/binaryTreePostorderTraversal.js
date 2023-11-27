e4d6e37263b73cdf39d06c2a3f8faa470e34c3f3
manfredjoa <joamanfred@gmail.com>
2023-11-27 15:58:42 -0500

    add binaryTreePostorderTraversal.js

---
+++ LeetCode/Easy/binaryTreePostorderTraversal.js
@@ -0,0 +1,16 @@
+const postorderTraversal = (root) => {
+  const result = [];
+
+  const traverse = (node) => {
+    if (!node) return;
+
+    traverse(node.left);
+    traverse(node.right);
+
+    result.push(node.val);
+  };
+
+  traverse(root);
+
+  return result;
+};
