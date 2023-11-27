20029935031edf91f3863edb94d3fd059302f746
manfredjoa <joamanfred@gmail.com>
2023-11-27 12:34:59 -0500

    add powerOfTwo.js

---
+++ LeetCode/Easy/powerOfTwo.js
@@ -0,0 +1,10 @@
+// Prompt
+// Given an integer n, return true if it is a power of two.
+// Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2 ^ x.
+
+const isPowerOfTwo = (n) => {
+  if (Number.isInteger(Math.log2(n))) return true;
+  else return false;
+};
