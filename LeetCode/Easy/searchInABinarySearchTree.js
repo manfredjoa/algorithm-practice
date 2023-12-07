4ce7365bc3bc808f4ae9a90f9a63b076d5793b8e
manfredjoa <joamanfred@gmail.com>
2023-12-07 15:36:55 -0500

    add searchInABinarySearchTree.js

---
+++ LeetCode/Easy/searchInABinarySearchTree.js
@@ -0,0 +1,13 @@
+// Prompt
+// You are given the root of a binary search tree (BST) and an integer val.
+
+// Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+// If such a node does not exist, return null.
+
+const searchBST = (root, val) => {
+  if (!root) return null;
+  else if (root.val === val) return root;
+
+  if (val < root.val) return searchBST(root.left, val);
+  else return searchBST(root.right, val);
+};
