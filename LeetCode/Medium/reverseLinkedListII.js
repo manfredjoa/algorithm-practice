ec923281ab21a773ace48e3192d6c45cb5ab9cec
manfredjoa <joamanfred@gmail.com>
2023-12-07 13:47:55 -0500

    add iterative one pass solution

--- LeetCode/Medium/reverseLinkedListII.js
+++ LeetCode/Medium/reverseLinkedListII.js
@@ -1,7 +1,8 @@
 // Prompt
 // Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 
-const reverseBetween = (head, left, right) => {
+// Recursive Solution
+const reverseBetweenRecursive = (head, left, right) => {
   const stack = [];
   let curr = head;
   let i = 1;
@@ -33,4 +34,44 @@ const reverseBetween = (head, left, right) => {
   }
 
   return head;
+};
+
+// Iterative Solution (One Pass)
+const reverseBetweenIterative = (head, left, right) => {
+  if (left === right) return head;
+
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let curr = head;
+  let prev = dummy;
+  let subListPrev = dummy;
+  let subListEnd = dummy;
+  let i = 1;
+
+  while (curr) {
+    if (i < left) {
+      subListPrev = curr;
+      prev = curr;
+      curr = curr.next;
+    } else if (i === left) {
+      subListEnd = curr;
+      prev = curr;
+      curr = curr.next;
+    } else if (i > left && i < right) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    } else {
+      if (subListPrev) subListPrev.next = curr;
+      else dummy.next = curr;
+
+      subListEnd.next = curr.next;
+      curr.next = prev;
+
+      return dummy.next;
+    }
+
+    i++;
+  }
 };
