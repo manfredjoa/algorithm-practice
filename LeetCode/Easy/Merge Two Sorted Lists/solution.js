8e1ee29d548be34faf286d4b2f14be9c0d9ec591
manfredjoa <joamanfred@gmail.com>
2024-01-01 16:25:15 -0500

    add Merge Two Sorted Lists

--- LeetCode/Easy/Merge Two Sorted Lists/solution.js
+++ LeetCode/Easy/Merge Two Sorted Lists/solution.js
@@ -1,11 +1,3 @@
-// Prompt
-// You are given the heads of two sorted linked lists list1 and list2.
-
-// Merge the two lists into one sorted list.
-// The list should be made by splicing together the nodes of the first two lists.
-
-// Return the head of the merged linked list.
-
 const mergeTwoLists = (list1, list2) => {
   if (!list2) return list1;
   else if (!list1) return list2;
