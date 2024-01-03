3e0341cef3b7820452016506c68f0abced9927d8
manfredjoa <joamanfred@gmail.com>
2024-01-03 14:38:25 -0500

    add Kth Largest Element in an Array

---
+++ LeetCode/Medium/Kth Largest Element in an Array/solution.py
@@ -0,0 +1,13 @@
+import heapq
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = [-num for num in nums]
+
+        heapify(heap)
+
+        for i in range(k - 1):
+            heappop(heap)
+
+        return -heappop(heap)
