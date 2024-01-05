e35c4ab57d54ef434a3a968647633e3c573edc37
manfredjoa <joamanfred@gmail.com>
2024-01-05 16:12:28 -0500

    add js solution

---
+++ LeetCode/Medium/Find the Duplicate Number/solution.js
@@ -0,0 +1,20 @@
+const findDuplicate = (nums) => {
+  let slow = nums[0],
+    fast = nums[0];
+
+  while (true) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+
+    if (slow === fast) break;
+  }
+
+  slow = nums[0];
+
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+
+  return slow;
+};
