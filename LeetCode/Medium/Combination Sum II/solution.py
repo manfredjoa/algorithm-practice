a550c9efdb661b425186c44bffc669a235c0c34a
manfredjoa <joamanfred@gmail.com>
2024-01-21 18:56:18 -0500

    make variable names pythonic

--- LeetCode/Medium/Combination Sum II/solution.py
+++ LeetCode/Medium/Combination Sum II/solution.py
@@ -4,21 +4,21 @@ class Solution:
 
         candidates.sort()
 
-        def dfs(i, currArr, currSum):
-            if currSum == target:
-                result.append(currArr.copy())
+        def dfs(i, curr_list, curr_sum):
+            if curr_sum == target:
+                result.append(curr_list.copy())
                 return
-            elif i >= len(candidates) or currSum > target:
+            elif i >= len(candidates) or curr_sum > target:
                 return
 
-            currArr.append(candidates[i])
-            dfs(i + 1, currArr, currSum + candidates[i])
+            curr_list.append(candidates[i])
+            dfs(i + 1, curr_list, curr_sum + candidates[i])
 
             while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
                 i += 1
 
-            currArr.pop()
-            dfs(i + 1, currArr, currSum)
+            curr_list.pop()
+            dfs(i + 1, curr_list, curr_sum)
 
         dfs(0, [], 0)
 
--- LeetCode/Medium/Combination Sum/solution.py
+++ LeetCode/Medium/Combination Sum/solution.py
@@ -2,18 +2,18 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
 
-        def dfs(i, curr_arr, curr_sum):
+        def dfs(i, curr_list, curr_sum):
             if curr_sum == target:
-                result.append(curr_arr.copy())
+                result.append(curr_list.copy())
                 return
             elif i >= len(candidates) or curr_sum > target:
                 return
 
-            curr_arr.append(candidates[i])
-            dfs(i, curr_arr, curr_sum + candidates[i])
+            curr_list.append(candidates[i])
+            dfs(i, curr_list, curr_sum + candidates[i])
 
-            curr_arr.pop()
-            dfs(i + 1, curr_arr, curr_sum)
+            curr_list.pop()
+            dfs(i + 1, curr_list, curr_sum)
 
         dfs(0, [], 0)
 
