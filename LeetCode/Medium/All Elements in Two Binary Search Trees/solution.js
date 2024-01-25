3166ac1d0f1656be270a353feb061f0470ad8b6f
manfredjoa <joamanfred@gmail.com>
2024-01-24 23:34:51 -0500

    add All Elements in Two Binary Search Trees

--- LeetCode/Medium/All Elements in Two Binary Search Trees/solution.js
+++ LeetCode/Medium/All Elements in Two Binary Search Trees/solution.js
@@ -1,6 +1,3 @@
-// Prompt
-// Given two binary search trees root1 and root2, return a list containing all the integers from both trees sorted in ascending order.
-
 const getAllElements = (root1, root2) => {
   const values = [];
 
