0aa6ea52deb49457e5f71d9a5057fa23c8ef1e5d
manfredjoa <joamanfred@gmail.com>
2024-02-09 14:35:28 +0800

    add Maximum Average Subarray I

--- LeetCode/Easy/Maximum Average Subarray I/solution.js
+++ LeetCode/Easy/Maximum Average Subarray I/solution.js
@@ -1,9 +1,3 @@
-// Prompt
-// You are given an integer array nums consisting of n elements, and an integer k.
-
-// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.
-// Any answer with a calculation error less than 10 - 5 will be accepted.
-
 const findMaxAverage = (nums, k) => {
   let maxSum = 0;
 
