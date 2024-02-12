bc72331598d3fa162053392db38c61996518581c
manfredjoa <joamanfred@gmail.com>
2024-02-12 19:49:19 +0800

    add Middle of the Linked List

--- LeetCode/Easy/Middle of the Linked List/solution.js
+++ LeetCode/Easy/Middle of the Linked List/solution.js
@@ -1,8 +1,3 @@
-// Prompt
-// Given the head of a singly linked list, return the middle node of the linked list.
-
-// If there are two middle nodes, return the second middle node.
-
 const middleNode = (head) => {
   const values = [];
   let curr = head;
